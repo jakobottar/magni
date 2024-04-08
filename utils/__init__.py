69ddcbfb526c29fb0ff480c9decae59a14e7ace4
jakobottar <jakobottar@pm.me>
2024-04-08 14:35:44 -0600

    add txt and raw (sortof) file parsing capability

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,5 @@ from .convnext import ConvNeXt
 from .data import ROUTES, get_datasets
 from .resnet import ResNet18, ResNet50
 from .vit import ViT
-from .xrd import parse_xml_file, process_xrd_data, read_brml
+from .xrd import (parse_xml_file, process_xrd_data, read_brml, read_raw,
+                  read_txt)
--- utils/xrd.py
+++ utils/xrd.py
@@ -11,8 +11,7 @@ COLS = "?,??,TwoTheta,Theta,Intensity"
 X_MIN = 10
 X_MAX = 90
 NUM_POINTS = 4096
-
-
+    
 def parse_xml_file(filename) -> pd.DataFrame:
 
     ## load XML file
@@ -27,6 +26,12 @@ def parse_xml_file(filename) -> pd.DataFrame:
     data = pd.read_csv(io.StringIO('\n'.join(data)))
 
     return data
+
+def read_raw(filename) -> pd.DataFrame:
+    
+    print("Not feasablem, as the file format is not publically available\n use 'jade pattern converter' instead: https://www.icdd.com/jade-pattern-converter/")      
+
+    return None
 
 def read_brml(filename, tmp_folder="./tmp") -> pd.DataFrame:
 
@@ -47,6 +52,31 @@ def read_brml(filename, tmp_folder="./tmp") -> pd.DataFrame:
     os.system(f"rm -rf {tmp_folder}")
 
     ## return the data
+    return data
+
+def read_txt(filename) -> pd.DataFrame:
+
+    ## read the first line of the text file:
+    with open(filename, "r") as f:
+        first_line = f.readline()
+        
+        ## there's a couple different txt file formats in here
+        if ";RAW4.00" in first_line:
+            data_start_line = [i for i, line in enumerate(f) if line.rstrip() == "[Data]"][0] + 3 # skip 3 lines past [Data]
+            data = pd.read_csv(filename, skiprows=data_start_line, sep=",", names=["TwoTheta", "Intensity", "x"])
+            data = data.drop(columns=["x"])
+        
+        elif "[°2Th.]" in first_line:
+            data = pd.read_csv(filename, sep="\t", skiprows=1, names=["TwoTheta", "Intensity","x", "xx", "xxx", "xxxx", "xxxxx"])
+            data = data.drop(columns=["x", "xx", "xxx", "xxxx", "xxxxx"])
+        
+        elif "Angle" in first_line or "Commander Sample ID" in first_line:
+            data = pd.read_csv(filename, sep=r"\s+", skiprows=1, names=["TwoTheta", "Intensity"])
+
+        elif "PDF#" in first_line:
+            data = pd.read_csv(filename, sep=r"\s+", skiprows=16)
+            data = data.loc[:, ['2-Theta', 'I(f)']].rename(columns={"2-Theta": "TwoTheta", "I(f)": "Intensity"})
+
     return data
 
 def process_xrd_data(data: pd.DataFrame) -> pd.DataFrame:
